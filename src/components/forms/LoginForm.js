0a938c90c2cc057837562a45102f674a07a4c44d
Kausik Das <kausik007007@gmail.com>
2021-08-16 16:59:06 +0530

    form closing issue resolved

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -33,7 +33,7 @@ const LoginForm = () => {
     // eslint-disable-next-line no-unused-expressions
     // loginFormClose;
     // history.push("/shop/all");
-    await window.location.reload();
+    window.location.reload();
   };
 
   console.log(errors);
